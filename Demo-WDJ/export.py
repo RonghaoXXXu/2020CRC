4c6e8a780af52d755b87b7ca3838d2c6aa454397
Youngho-Hsu <61378371+Youngho-Hsu@users.noreply.github.com>
2020-08-03 19:53:10 +0800

    Add files via upload

---
+++ Demo-WDJ/export.py
@@ -0,0 +1,5 @@
+python3 export_inference_graph.py\
+ --input_type image_tensor\
+ --pipeline_config_path training/ssd_mobilenet_v1_coco.config\
+ --trained_checkpoint_prefix training/model.ckpt-6139\
+ --output_directory WDJ_Dete

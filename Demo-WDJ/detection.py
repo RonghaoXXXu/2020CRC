09f22ff7f5d88436820435125aa3b7b46143d9bf
Ronghao-Xu <61378371+Ronghao-Xu@users.noreply.github.com>
2020-08-23 21:44:20 +0800

    Update detection.py

--- Demo-WDJ/detection.py
+++ Demo-WDJ/detection.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+# # Object Detection Demo
+# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
+
+
+from distutils.version import StrictVersion
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -15,8 +22,8 @@ from PIL import Image
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
 
-if tf.__version__ < '1.4.0':
-  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+# if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+#   raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
 
 # ## Env setup
@@ -25,13 +32,12 @@ if tf.__version__ < '1.4.0':
 
 
 # This is needed to display the images.
-
+# get_ipython().magic(u'matplotlib inline')
 
 
 # ## Object detection imports
 # Here are the imports from the object detection module.
 
-# In[3]:
 
 
 from utils import label_map_util
@@ -43,7 +49,7 @@ from utils import visualization_utils as vis_util
 
 # ## Variables
 # 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
+# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_FROZEN_GRAPH` to point to a new .pb file.  
 # 
 # By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 
@@ -51,36 +57,41 @@ from utils import visualization_utils as vis_util
 
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2018_01_28'
-#MODEL_FILE = MODEL_NAME + '.tar.gz'
-#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+MODEL_NAME = 'zhangjn_detction'
+# MODEL_FILE = MODEL_NAME + '.tar.gz'
+# DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join('data', 'zhangjn.pbtxt')
 
-NUM_CLASSES = 90
+NUM_CLASSES = 1
 
 
 # ## Download Model
 
-# In[ ]:
 
 
-
-
+# opener = urllib.request.URLopener()
+# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+'''
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
+'''
 
 # ## Load a (frozen) Tensorflow model into memory.
 
-# In[5]:
 
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
@@ -89,7 +100,6 @@ with detection_graph.as_default():
 # ## Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 
-# In[6]:
 
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
@@ -99,7 +109,7 @@ category_index = label_map_util.create_category_index(categories)
 
 # ## Helper code
 
-# In[7]:
+# In[8]:
 
 
 def load_image_into_numpy_array(image):
@@ -110,7 +120,6 @@ def load_image_into_numpy_array(image):
 
 # # Detection
 
-# In[8]:
 
 
 # For the sake of simplicity we will use only 2 images:
@@ -118,14 +127,13 @@ def load_image_into_numpy_array(image):
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
 PATH_TO_TEST_IMAGES_DIR = 'test_images'
-#TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-TEST_IMAGE_PATHS = os.listdir('/home/youngho/models/research/object_detection/test_images')
-os.chdir('/home/youngho/models/research/object_detection/test_images')
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(3, 8) ]
+
 # Size, in inches, of the output images.
-IMAGE_SIZE = (12,8)
+IMAGE_SIZE = (12, 8)
 
 
-# In[9]:
+# In[10]:
 
 
 def run_inference_for_single_image(image, graph):
@@ -175,7 +183,7 @@ def run_inference_for_single_image(image, graph):
   return output_dict
 
 
-# In[10]:
+# In[ ]:
 
 
 for image_path in TEST_IMAGE_PATHS:
@@ -196,6 +204,7 @@ for image_path in TEST_IMAGE_PATHS:
       category_index,
       instance_masks=output_dict.get('detection_masks'),
       use_normalized_coordinates=True,
-      line_thickness=5)
+      line_thickness=8)
   plt.figure(figsize=IMAGE_SIZE)
   plt.imshow(image_np)
+  plt.show()
